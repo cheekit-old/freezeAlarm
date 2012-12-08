172be3bad5ccc897344551932b665e9900cece88
KOMATSU Issei <issei@mizuhataya.com>
2012-12-08 19:50:12 +0900

    Notificationを追加

--- src/server/frapi/custom/Batch/SendNotification.php
+++ src/server/frapi/custom/Batch/SendNotification.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once '../Model/Notification.php';
 require_once '../Model/Forecast.php';
 
@@ -18,10 +19,10 @@ $result = $stmt->fetchAll();
 // PushNotification 準備
 $push = new ApnsPHP_Push(
     ApnsPHP_Abstract::ENVIRONMENT_SANDBOX,
-    // ApnsPHP_Abstract::ENVIRONMENT_PRODUCTION,
-    '../Config/FreezeAlarm.pem'
+    '../Config/apns-dev.pem'
 );
-$push->setRootCertificationAuthority('../Config/erca.pem');
+$push->setRootCertificationAuthority('../Config/apns-dev-cert.pem');
+
 $push->connect();
 
 foreach( $result as $target ){
@@ -30,22 +31,19 @@ foreach( $result as $target ){
     $tomorrow = date("Ymd",strtotime("+1 day"));
     $ret = $forecast->retrieveTemperature($tomorrow);
 
-    $text = "test";
-#    $min = $ret["temperature"]["min"];
-#    if($min <= -4){
-#        $text = "【警告】最低気温 " . $min . " 度";
-#    }else if($min < 0){
-#        $text = "【注意】最低気温 " . $min . " 度";
-#    }else{
-#        continue;
-#    }
-    #
+    $min = $ret["temperature"]["min"];
+    if($min <= -4){
+        $text = "【警告】最低気温 " . $min . " 度"; // TODO: メッセージ
+    }else if($min < 0){
+        $text = "【注意】最低気温 " . $min . " 度"; // TODO: メッセージ
+    }else{
+        continue;
+    }
+
     try {
         $message = new ApnsPHP_Message($target['device_id']);
         $message->setText($text);
-    #    $message->setExpiry(30);
-        $message->setCustomProperty('acme2', array('bang', 'whiz'));
-        $message->setCustomProperty('acme3', array('bing', 'bong'));
+        $message->setExpiry(30);
         $push->add($message);
     } catch (Exception $e){
         continue;
@@ -58,6 +56,7 @@ foreach( $result as $target ){
     try {
         $push->send();
     } catch (Exception $e){
+        error_log($e->getMessage());
     }
 $push->disconnect();
 
